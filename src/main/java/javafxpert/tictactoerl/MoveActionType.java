24a3855bc6ccab6199f54ff5abe57daa4209df00
LazyCatDoll <139642833+LazyCatDoll@users.noreply.github.com>
2022-09-27 20:27:32 +0000

    Add CHANGELOG

--- src/main/java/javafxpert/tictactoerl/MoveActionType.java
+++ src/main/java/javafxpert/tictactoerl/MoveActionType.java
@@ -26,4 +26,4 @@ import java.util.List;
  * @author James L. Weaver (Twitter: @JavaFXpert)
  */
 public class MoveActionType implements ActionType {
-  public static String BASE
+  public static String BASE_ACTION_NA
