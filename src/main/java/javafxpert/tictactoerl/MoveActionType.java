3bcc36f02c9daa892bbfaa8a184902d4d663d036
LazyCatDoll <139642833+LazyCatDoll@users.noreply.github.com>
2022-10-15 13:03:48 +0000

    Day 5

--- src/main/java/javafxpert/tictactoerl/MoveActionType.java
+++ src/main/java/javafxpert/tictactoerl/MoveActionType.java
@@ -35,4 +35,4 @@ public class MoveActionType implements ActionType {
 
   @Override
   public Action associatedAction(String strRep) {
-    return new MoveAction(0); //TODO:
+    return new MoveAction(0); //TODO: Ascer
