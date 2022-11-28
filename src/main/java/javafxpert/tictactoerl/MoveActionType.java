4dead300cb9b7f0583c9bad1fdf7398a368b7953
LazyCatDoll <139642833+LazyCatDoll@users.noreply.github.com>
2022-11-28 14:47:25 +0000

    Updated variable name

--- src/main/java/javafxpert/tictactoerl/MoveActionType.java
+++ src/main/java/javafxpert/tictactoerl/MoveActionType.java
@@ -44,4 +44,4 @@ public class MoveActionType implements ActionType {
     TicTacToeState tttState = (TicTacToeState)state;
 
     String gameStatus = (String)tttState.get(TicTacToeState.VAR_GAME_STATUS);
-    if (gameStatus.equals(Tic
+    if (gameStatus.equals(TicTacTo
