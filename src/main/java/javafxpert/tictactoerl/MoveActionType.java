94459d3ae63e4ff75a24ddeaba224334c5b5c82d
LazyCatDoll <139642833+LazyCatDoll@users.noreply.github.com>
2022-12-11 16:40:57 +0000

    fix grammar

--- src/main/java/javafxpert/tictactoerl/MoveActionType.java
+++ src/main/java/javafxpert/tictactoerl/MoveActionType.java
@@ -45,4 +45,4 @@ public class MoveActionType implements ActionType {
 
     String gameStatus = (String)tttState.get(TicTacToeState.VAR_GAME_STATUS);
     if (gameStatus.equals(TicTacToeState.GAME_STATUS_IN_PROGRESS)) {
-      String gameBoard = (String)tttState.g
+      String gameBoard = (String)tttState.get(TicTacToeS
