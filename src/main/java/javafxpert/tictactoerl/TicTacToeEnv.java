cc15b58e5ebf5ae2f7e95ae4a773fdeca3f64dfd
LazyCatDoll <139642833+LazyCatDoll@users.noreply.github.com>
2023-01-15 12:28:57 +0000

    Update book.json

--- src/main/java/javafxpert/tictactoerl/TicTacToeEnv.java
+++ src/main/java/javafxpert/tictactoerl/TicTacToeEnv.java
@@ -147,3 +147,17 @@ public class TicTacToeEnv implements Environment, EnvironmentServerInterface {
       terminated = true;
     }
     else if (gameStatus.toCharArray()[0] == opposingPlayerMark) {
+
+      // TODO: Consider removing this condition, as it doen't seem possible to encounter
+      reward = WIN_REWARD;
+      terminated = true;
+    }
+    else if (gameStatus.equals(TicTacToeState.GAME_STATUS_CATS_GAME)) {
+      reward = MOVE_REWARD;
+      terminated = true;
+    }
+    else {
+      reward = MOVE_REWARD;
+      terminated = false;
+
+      // Uncomment to employ a strategy of playing the first empty cell with an "O"
