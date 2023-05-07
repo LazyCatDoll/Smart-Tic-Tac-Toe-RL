8c38ecbef8545595385640138149a8add12d47ed
LazyCatDoll <139642833+LazyCatDoll@users.noreply.github.com>
2023-05-07 17:48:18 +0000

    2.24.0

--- src/main/java/javafxpert/tictactoerl/TicTacToeQLearning.java
+++ src/main/java/javafxpert/tictactoerl/TicTacToeQLearning.java
@@ -2,4 +2,4 @@
  * Copyright 2016 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this fil
+ * you may not use this file excep
