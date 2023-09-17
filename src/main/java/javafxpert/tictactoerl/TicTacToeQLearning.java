5d51fae9d1ef380f03251f7ff3b0525d7af877ba
LazyCatDoll <139642833+LazyCatDoll@users.noreply.github.com>
2023-09-17 14:58:01 +0000

    updated docstring

--- src/main/java/javafxpert/tictactoerl/TicTacToeQLearning.java
+++ src/main/java/javafxpert/tictactoerl/TicTacToeQLearning.java
@@ -17,4 +17,4 @@ package javafxpert.tictactoerl;
 
 import burlap.behavior.singleagent.Episode;
 import burlap.behavior.singleagent.auxiliary.EpisodeSequenceVisualizer;
-import burlap.behavior.singlea
+import burlap.behavior.singleagent.learn
