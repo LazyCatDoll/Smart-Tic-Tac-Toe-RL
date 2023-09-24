6ce06d24f5c79823f7871fa6ef1993349ebc00d7
LazyCatDoll <139642833+LazyCatDoll@users.noreply.github.com>
2023-09-24 19:31:26 +0000

    Rename images

--- src/main/java/javafxpert/tictactoerl/TicTacToeQLearning.java
+++ src/main/java/javafxpert/tictactoerl/TicTacToeQLearning.java
@@ -18,4 +18,4 @@ package javafxpert.tictactoerl;
 import burlap.behavior.singleagent.Episode;
 import burlap.behavior.singleagent.auxiliary.EpisodeSequenceVisualizer;
 import burlap.behavior.singleagent.learning.LearningAgent;
-import b
+import burlap.be
